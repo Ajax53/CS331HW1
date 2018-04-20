5f399b4c189d33e2343238a5edf4b72e8d598e50
Andrew Waite <waitean@oregonstate.edu>
2018-04-19 19:14:10 -0700

    Adding in psuedo-code templates and necessary includes.

--- WandC.c
+++ WandC.c
@@ -1,4 +1,38 @@
+#include <vector> //For the priority queue generation
+#include <queue>  //For the priority queue generation
+#include <cstdio> //For fprintf and FILE* streams. 
+
+class Node {
+     public:
+	  int lChickens;
+	  int lWolves;
+	  int lBoats;
+	  int rChickens;
+	  int rWolves;
+	  int rBoats;
+	  Node* parent;
+	  int priority;
+};
+
+//Four evaluation functions.
+//     Returning a string that represents the path to the optimal solution.
+//Four successor functions.
+//     Returns the nodes that can be reached from a given node. 
+//A single is goal node
+//     bool matches(Node n, Node g)
 
 int main(int argc, char** argv) {
+     //Get the intial nodes
+     //Get the goal node.
+     //Switch on the modde
+     //     BFS
+     //          call BFSEval
+     //     DFS
+     //          call DFSEval
+     //     IDDFS
+     //          call IDDFSEval
+     //     A*
+     //          call A*Eval
+     //Print the solution to stdout and to a file. 
      return 0;
 }
