476f20e101d801990014364492b4e1f58b64cc00
Andrew Waite <waitean@oregonstate.edu>
2018-04-20 08:47:08 -0700

    Added some exta stuff to Node, started implementation for evaluation functions.

--- WandC.c
+++ WandC.c
@@ -1,12 +1,11 @@
 #include <vector>  //For the priority queue generation
 #include <queue>   //For the priority queue generation
 #include <cstdio>  //For fprintf and FILE* streams.
-//#include <stdio.h> //For strtok this is cstdio
-//#include <string.h> //For strtok this is cstring
 #include <cstdio>  //For fprintf and FILE* streams.
-#include <cstring> //For the strtok. In c++ you want to include these instead of .h files
+#include <cstring> //For the strtok. 
 #include <string>  //For the toString function. And generally passing things around.
 #include <map>     //For the already expanded nodes container.
+#include <functional> //For the comparator functions for the priority queue
 
 class Node {
      public:
@@ -23,6 +22,8 @@ class Node {
 	  int rBoats;
 	  Node* parent;
 	  int priority;
+	  int depth;
+
 	  std::string toString();
 };
 
@@ -34,18 +35,39 @@ struct Successor {
      Node ww;
 };
 
+//Comparison functions
+bool bfsCmp(Node lhs, Node rhs) { return lhs.depth < rhs.depth; }
+bool dfsCmp(Node lhs, Node rhs) { return lhs.depth > rhs.depth; }
+
 //Four evaluation functions.
 //     Returning a string that represents the path to the optimal solution.
 //     The returned string should be a composition of all of the toStrings
 //     from the found goal node all the way back up to the root node.
-//     Expanded node list maps std::string to Node[5]
+//     Expanded node list maps std::string to Node[5] (struct Successor)
+std::string bfsEval(Node initialNode, Node goalNode);
+std::string dfsEval(Node initialNode, Node goalNode);
+std::string iddfsEval(Node initialNode, Node goalNode);
+std::string astarEval(Node initialNode, Node goalNode);
+
 //Four successor functions.
 //     Returns the nodes that can be reached from a given node.
-//     This will return a Successor type
+//     This will return a Successor struct
+
+struct Successor bfsSuccessor(Node parent);
+struct Successor dfsSuccessor(Node parent);
+struct Successor iddfsSuccessor(Node parent);
+struct Successor astarSuccessor(Node parent);
+
 //A single is goal node
 //     bool matches(Node n, Node g)
 
 int main(int argc, char** argv) {
+     if (argc != 5) {
+     	fprintf(stdout, "Please enter the WandC command in the following format:\n");
+	fprintf(stdout, "\tWandC <initial state file> <goal state file> <mode> <output file>\n");
+	return 1;
+     }
+     
      FILE * filePointer;
      char leftChars[50];
      char rightChars[50];
@@ -72,19 +94,26 @@ int main(int argc, char** argv) {
      Node goalNode(leftChars, rightChars);
      fclose(filePointer);
 
-     fprintf(stdout, "%s\n%s\n", initNode.toString().c_str(), goalNode.toString().c_str());
-
-
      //Switch on the modde
-     //     BFS
-     //          call BFSEval
-     //     DFS
-     //          call DFSEval
-     //     IDDFS
-     //          call IDDFSEval
-     //     A*
-     //          call A*Eval
+     switch (argv[3][0]) {
+	  case 'b':
+	       fprintf(stdout, "BFS!\n");
+	       break;
+	  case 'd':
+	       fprintf(stdout, "DFS!\n");
+	       break;
+	  case 'i':
+               fprintf(stdout, "IDDFS!\n");
+	       break;
+	  case 'a':
+	       fprintf(stdout, "ASTAR!\n");
+	       break;
+	  default:
+	       fprintf(stdout, "Please enter a valid mode. (bfs, dfs, iddfs, astar)\n");
+     }
+    
      //Print the solution to stdout and to a file.
+     
      return 0;
 }
 
@@ -98,6 +127,7 @@ Node::Node() {
      rBoats = 0;
      parent = NULL;
      priority = 0;
+     depth = 0;
 }
 
 Node::Node(int lc, int lw, int lb, int rc, int rw, int rb, Node *pp, int p) {
@@ -110,6 +140,7 @@ Node::Node(int lc, int lw, int lb, int rc, int rw, int rb, Node *pp, int p) {
      rBoats = rb;
      parent = pp;
      priority = p;
+     depth = pp->depth + 1;
 }
 
 Node::Node(char leftChars[50], char rightChars[50]) {
@@ -136,6 +167,7 @@ Node::Node(char leftChars[50], char rightChars[50]) {
 
      parent = NULL;
      priority = 0;
+     depth = 0;
 }
 
 std::string Node::toString() {
@@ -154,3 +186,31 @@ std::string Node::toString() {
      s += "\n";
      return s;
 }
+
+std::string bfsEval(Node initialNode, Node goalNode) {
+     std::string returnString("");
+     int nodesExpanded = 0, solutionLength;
+     
+     if (isGoal(initialNode, goalNode)) {
+          solutionLength = 1;
+          returnString += initialNode.toString();
+     } else {
+          //Make the expanded nodes map
+	  std::map<std::string, struct Successor> expendedNodes;
+
+	  //Make the priority queue.
+	  std::priority_queue<Node, std::vector<Node>, std::function<bool(Node, Node)>> fringe(bfsCmp);
+	  //loop over the priority queue until we find a solution on the queue. 
+     }
+
+     returnString += "\nnodes expanded: ";
+     returnString += std::to_string(nodesExpanded);
+     returnString += "\nnodes in solution: ";
+     returnString += std::to_string(solutionLength);
+     returnString += "\n";
+
+     return returnString;
+}
+
+
+//  fprintf(stdout, "%s\n%s\n", initNode.toString().c_str(), goalNode.toString().c_str());
